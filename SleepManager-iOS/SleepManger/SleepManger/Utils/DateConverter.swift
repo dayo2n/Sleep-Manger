c973ca82c23f7ef3411450c91a0663ac91002fcb
dayo2n <0217dayun@naver.com>
2022-06-07 14:40:24 +0900

    [fix] uct to kst #2

--- SleepManager-iOS/SleepManger/SleepManger/Utils/DateConverter.swift
+++ SleepManager-iOS/SleepManger/SleepManger/Utils/DateConverter.swift
@@ -7,25 +7,34 @@
 
 import Foundation
 
-func Date2String(date: Date) -> [String] {
-    var newGoals : [String] = [] // [new sleep goal time hour, min, new wake-up goal time hour, min]
+// Date 타입의 값을 "00:00"으로 이루어진 String 타입으로 리턴
+func Date2String(date: Date) -> String {
     
     let dateFormatter = DateFormatter()
     dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
-    dateFormatter.timeZone = TimeZone(identifier: "UTC")
+    dateFormatter.timeZone = TimeZone(identifier: "KST")
     let splitOnlyTime = dateFormatter.string(from: date).split(separator: " ")
     let splitNewTime = splitOnlyTime[1].split(separator: ":")
-    newGoals.append("\(splitNewTime[0])")
-    newGoals.append("\(splitNewTime[1])")
     
-    return newGoals
+    return "\(splitNewTime[0]):\(splitNewTime[1])"
 }
 
+// "00:00" 형식의 문자열을 "2000년 1월 1일의 00:00:00"의 Date 타입으로 리턴
 func TimeString2Date(date: String) -> Date {
     let splitTime = date.split(separator: ":")
     let stringTime = "2000-01-01 \(splitTime[0]):\(splitTime[1]):00"
     let dateFormatter = DateFormatter()
     dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
-    dateFormatter.timeZone = TimeZone(identifier: "UTC")
+    dateFormatter.timeZone = TimeZone(identifier: "KST")
     return dateFormatter.date(from: stringTime) ?? Date()
+}
+
+// Date 타입에서 "2000-01-01" 형식의 문자열로 리턴
+func Date2OnlyDate(date: Date) -> String {
+    let dateFormatter = DateFormatter()
+    dateFormatter.dateFormat = "yyyy-MM-dd HH:mm:ss"
+    dateFormatter.timeZone = TimeZone(identifier: "KST")
+    let splitOnlyDate = dateFormatter.string(from: date).split(separator: " ")
+    
+    return "\(splitOnlyDate[0])"
 }
