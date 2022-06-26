4d7ea76a2fb29980ca052765842e1ede36b687fb
mk-kang1 <71447571+mk-kang1@users.noreply.github.com>
2022-06-26 23:03:14 +0900

    수분 섭취 알림 시간 관련 기능들 구현 (#3) , add comment

--- backend/src/main/java/cnu/routinemanager/exception/DrinkException.java
+++ backend/src/main/java/cnu/routinemanager/exception/DrinkException.java
@@ -2,6 +2,7 @@ package cnu.routinemanager.exception;
 
 import org.springframework.http.HttpStatus;
 
+// 수분 섭취 관련  Exception Handler
 public class DrinkException extends SleepManagerException {
     public DrinkException(String message, HttpStatus httpStatus) {
         super(message, httpStatus);
