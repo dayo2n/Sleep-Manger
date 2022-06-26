42a2c230a2a3426b318e223e1b099045576c96f3
mk-kang1 <71447571+mk-kang1@users.noreply.github.com>
2022-06-26 23:11:03 +0900

    [feat] 수분 섭취량 조회 기능 구현 , add comment

--- backend/src/main/java/cnu/routinemanager/repository/DrinkRepository.java
+++ backend/src/main/java/cnu/routinemanager/repository/DrinkRepository.java
@@ -7,8 +7,12 @@ import java.time.LocalDate;
 import java.util.List;
 import java.util.Optional;
 
+// 수분 섭취 기록 관련 데이터 베이스
 public interface DrinkRepository extends JpaRepository<Drink, Long> {
+    // 회원의 Id와 해당 날짜 수분 기록이 존재하는지
     boolean existsByMemberIdAndDate(Long id, LocalDate date);
+    // 회원 Id와 날짜 찾기 
     Optional<Drink> findByMemberIdAndDate(Long memberId, LocalDate date);
+    // 회원의 특정 기간 동안의 수분 섭취 기록 찾기
     List<Drink> findByMemberIdAndDateBetweenOrderByDate(Long memberId, LocalDate startDate, LocalDate endDate);
 }
