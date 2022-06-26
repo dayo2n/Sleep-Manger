d3c4a333eae6c886f5d2a9055515936e7f77c104
mk-kang1 <71447571+mk-kang1@users.noreply.github.com>
2022-06-26 23:10:25 +0900

    수분 섭취 알림 시간 관련 기능들 구현 (#3) , add comment

--- backend/src/main/java/cnu/routinemanager/repository/DrinkNotificationTimeRepository.java
+++ backend/src/main/java/cnu/routinemanager/repository/DrinkNotificationTimeRepository.java
@@ -6,7 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import cnu.routinemanager.domain.DrinkNotificationTime;
 
+// 수분 섭취 알림 시간 관련 데이터 베이스
+
 public interface DrinkNotificationTimeRepository extends JpaRepository<DrinkNotificationTime, Long> {
+    //현재 Id가 존재하는지
     boolean existsByMemberId(Long memberId);
+    // 회원의 수분 섭취 알림 시간 찾기
     List<DrinkNotificationTime> findByMemberIdOrderByDrinkTime(Long memberId);
 }
