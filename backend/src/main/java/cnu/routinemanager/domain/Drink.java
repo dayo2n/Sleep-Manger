e4ffde078f5eb9989cebc71d8b5bb2af5c407daa
mk-kang1 <71447571+mk-kang1@users.noreply.github.com>
2022-06-26 20:40:26 +0900

    [feat] 수분 섭취량 기록, 추가 기록, 수정 기능 구현, add comment

--- backend/src/main/java/cnu/routinemanager/domain/Drink.java
+++ backend/src/main/java/cnu/routinemanager/domain/Drink.java
@@ -23,21 +23,24 @@ public class Drink {
 
     @Column
     private LocalDate date;
-
+    // member, 섭취량, 날짜
     public Drink(Member member, int amount, LocalDate date) {
         this.member = member;
         this.amount = amount;
         this.date = date;
     }
 
+    // 같은 날짜가 이미 존재하는지
     public boolean hasSameDate(LocalDate date) {
         return this.date.equals(date);
     }
 
+    // 기존 섭취량에 새로 들어온 섭취량을 더해줌
     public void addAmount(int amount) {
         this.amount += amount;
     }
 
+    // 기존 섭취량을 무시하고 새로 입력된 섭취량으로 수정
     public void modifyAmount(int amount) {
         this.amount = amount;
     }
