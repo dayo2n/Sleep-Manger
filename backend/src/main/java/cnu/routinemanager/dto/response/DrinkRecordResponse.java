88fe982a122c931b9dd5a2b2e1d2c6dccf807e49
mk-kang1 <71447571+mk-kang1@users.noreply.github.com>
2022-06-26 23:00:31 +0900

    [feat] 수분 섭취량 조회 기능 구현 , add comment

--- backend/src/main/java/cnu/routinemanager/dto/response/DrinkRecordResponse.java
+++ backend/src/main/java/cnu/routinemanager/dto/response/DrinkRecordResponse.java
@@ -4,6 +4,7 @@ import java.time.LocalDate;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 
+// Front 로 보낼때 데이터 format
 public class DrinkRecordResponse {
     @JsonFormat(pattern = "yyyy-MM-dd")
     private LocalDate date;
